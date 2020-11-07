8d3ed04fc0adaaf2450a9c6c8930a9e6d82d6068
kmimx <61816789+kmimx@users.noreply.github.com>
2020-11-07 17:56:01 +0800

    Update adguardhome.sh

--- trunk/user/adguardhome/adguardhome.sh
+++ trunk/user/adguardhome/adguardhome.sh
@@ -139,7 +139,7 @@ fi
 dl_adg(){
 logger -t "AdGuardHome" "下载AdGuardHome"
 #wget --no-check-certificate -O /tmp/AdGuardHome.tar.gz https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.104.0/AdGuardHome_linux_mipsle_softfloat.tar.gz
-curl -k -s -o /tmp/AdGuardHome/AdGuardHome --connect-timeout 10 --retry 3 https://raw.githubusercontent.com/kmimx/rt-n56u/master/trunk/user/adguardhome/AdGuardHome
+curl -k -s -o /tmp/AdGuardHome/AdGuardHome --connect-timeout 10 --retry 3 https://cdn.jsdelivr.net/gh/kmimx/rt-n56u/trunk/user/adguardhome/AdGuardHome
 #https://cdn.jsdelivr.net/gh/kmimx/rt-n56u/trunk/user/adguardhome/AdGuardHome
 if [ ! -f "/tmp/AdGuardHome/AdGuardHome" ]; then
 logger -t "AdGuardHome" "AdGuardHome下载失败，请检查是否能正常访问github!程序将退出。"
